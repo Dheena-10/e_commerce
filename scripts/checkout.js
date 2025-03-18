954fa4c37eda9941ef062f4937606ce5d95cacd2
Dheena-10 <dheenaoffl10@gmail.com>
2025-03-18 12:49:28 +0530

    save cart in localstorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -102,7 +102,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     removeFromCart(productId);
     
     const container = document.querySelector(
-      '.js-cart-item-container-${productId}'
+      `.js-cart-item-container-${productId}`
     );
     container.remove();
   });
